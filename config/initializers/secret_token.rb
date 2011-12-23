1e51618ee4dd25a8e045e773417e76456d2e77f0
next.big.thing <my.spare.inbox@live.com>
2011-12-23 22:36:56 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webapp::Application.config.secret_token = '054efe9a267e855d3bfebcc3e541dec2bcbdc43f7e7a108485849f98f4608a9f0b49649d690da6e8d7e435de347f19cf408c4e7a6e447db69bfa13fe73b586f9'
